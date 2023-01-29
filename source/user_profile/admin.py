e2a47e45f1fca0e4d09c987ea3095b5b059d7b42
violencedev <furkanesen39@gmail.com>
2023-01-29 23:59:04 +0300

    daily fix 3

--- source/user_profile/admin.py
+++ source/user_profile/admin.py
@@ -8,7 +8,8 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('get_account', 'web_theme', 'shop_bought_products', 'login_count', 'last_logged_in', 'total_point', 'get_created_date')
     list_filter = ('web_theme', 'shop_bought_products', 'login_count', 'last_logged_in', 'total_point')
     
-    def get_readonly_fields(self, request, obj=None):
+    @staticmethod
+    def get_readonly_fields(request, obj=None):
         if request.user.username == "FurkanEsen":
             return [f.name for f in obj._meta.fields if not f.editable]
         return [f.name for f in obj._meta.fields if not f.editable and f.name != "ip_addresses"]
